146e3c91ebb8b986f1148cd488d01f2327435034
ElvinAghdassi <ea22360@my.westlake.school.nz>
2026-04-28 10:38:50 +1200

    created library

--- OOP/ooptasks.py
+++ OOP/ooptasks.py
@@ -55,7 +55,7 @@ if __name__ == "__main__":
 """
 
 # Create a class to represent a rectangle, with attributes for width and height. The class should have methods for calculating the area and perimeter. 
-
+"""
 class Rectangle:
     def __init__(self, width, height):
         self.width = width
@@ -63,19 +63,27 @@ class Rectangle:
         pass
 
     def description(self):
-        return f"Rectangle with a width of {self.width} and a height of {self.height}"
+        print(f"Rectangle with a width of {self.width} and a height of {self.height}")
+        return self.width, self.height
+    
     def area_calc(self):
         area = self.width * self.height
+        print(f"Your Rectange has an area of {area} units^2")
         return area
     def perimeter_calc(self):
         perimeter = 2*self.width + 2*self.height
+        print(f"Your Rectange has a perimeter of {perimeter}")
         return perimeter
 
 if __name__ == "__main__":
     myRec = Rectangle(10, 10)
+    myRec.description()
+    myRec.area_calc()
+    myRec.perimeter_calc()
+"""
 
 #Create a class to represent a person, with attributes for first name, last name, and date of birth. The class should have methods for calculating the person's age. 
-
+"""
 class Person:
     def __init__(self, first_name, last_name, dob):
         self.first_name = first_name
@@ -84,25 +92,50 @@ class Person:
         pass
     def age_calc(self):
         age = 2026 - self.dob
+        print(f"You are {age} years old")
         return age
+    
+if __name__ == "__main__":
+    myPerson = Person("Elvin", "Aghdassi", 2008)
+    myPerson.age_calc()
+
+"""
     
 #Create a class to represent a library, with attributes for books and users. The class should have methods for checking out books, returning books, and keeping track of which books are currently checked out. 
 
 class Library:
-    books = []
-    users = []
     def __init__(self, books, users):
         self.books = books
         self.users = users
         pass
-    def track_book(self):
+    def track_book(self): 
+        print(f"{self.users} has checked out")
+        for books in self.books: 
+            print(books)
         return self.books
     
     def return_books(self):
+        print(f"\n{self.users}'s Books are")
+        for books in self.books:
+            print(books)
         user_input = input(str("Which book would you like to return?    "))
         self.books.append(user_input)
         return self.books
     
     def checkout_book(self):
-        print(self.books)
+        print(f"\n{self.users} checked out books are:")
+        for books in self.books:
+            print(books)
         user_input = input(str("Which book would you like to checkout?:   "))
+        self.books.remove(user_input)
+        print(f"\n{self.users} checked out books are now:")
+        for books in self.books:
+            print(books)
+        return self.books
+    
+if __name__ == "__main__":
+    myBooks = Library(["Tu", "1984", "Animal Farm"], "Elvin")
+    myBooks.track_book()
+    myBooks.return_books()
+    myBooks.checkout_book()
+
